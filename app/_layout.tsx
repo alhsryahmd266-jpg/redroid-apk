49c539dd3a59b8b221a8ee9eb419ee8690479b6d
alhsryahmd266-jpg <alhsryahmd266@gmail.com>
2026-06-13 09:07:57 +0300

    fix: remove problematic imports from _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,8 +9,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { KeyboardProvider } from "react-native-keyboard-controller";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { ErrorBoundary } from "@/components/ErrorBoundary";
@@ -50,11 +48,7 @@ export default function RootLayout() {
     <SafeAreaProvider>
       <ErrorBoundary>
         <QueryClientProvider client={queryClient}>
-          <GestureHandlerRootView>
-            <KeyboardProvider>
-              <RootLayoutNav />
-            </KeyboardProvider>
-          </GestureHandlerRootView>
+          <RootLayoutNav />
         </QueryClientProvider>
       </ErrorBoundary>
     </SafeAreaProvider>
