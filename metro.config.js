50de010ab1d45d7e66a15928ff099aee1dfcdfa0
alhsryahmd266-jpg <alhsryahmd266@gmail.com>
2026-07-09 19:04:33 +0300

    fix: broaden metro blockList regex for tmp dirs

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,8 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.blockList = [
   /jszip_tmp_[0-9]+\/.*/,
+  /fd-slicer_tmp_[0-9]+\/.*/,
+  /[a-zA-Z0-9_-]+_tmp_[0-9]+\/.*/,
 ];
 
 module.exports = config;
